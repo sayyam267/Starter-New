709417e1aca9a06941fde3c25571ecfaee9ad3fe
Zain Haroon <zainharoon890@gmail.com>
2022-06-05 10:42:05 +0500

    Update OAuth2.js

--- routes/OAuth2.js
+++ routes/OAuth2.js
@@ -34,31 +34,32 @@ router.post("/google/createuser", async (req, res) => {
       message: "Fetched",
     });
   } else if (existinguser?.source == "google") {
-    if(existinguser.isActive){
-    let user = {
-      id: existinguser._id,
-      fname: existinguser.fname,
-      lname: existinguser.lname,
-      email: existinguser.email,
-    };
-    const token = jwt.sign(user, process.env.PRIVATE_KEY);
-    return res.send({
-      data: {
-        token: token,
-        role: existinguser.userType,
-        profilePicture: existinguser.profilePicture,
-        name: existinguser.fname + " " + existinguser.lname,
+    if (existinguser.isActive) {
+      let user = {
+        id: existinguser._id,
+        fname: existinguser.fname,
+        lname: existinguser.lname,
         email: existinguser.email,
-        balance: existinguser.balance,
-      },
-      message: "Fetched",
-    });
-  }
-  else{
-    let e= new Error("User is Blocked from using TourBook Services");
-    e.statusCode=400;
-    return res.status(e?statusCode||400).send({data:null,message:e.message});
-  }
+      };
+      const token = jwt.sign(user, process.env.PRIVATE_KEY);
+      return res.send({
+        data: {
+          token: token,
+          role: existinguser.userType,
+          profilePicture: existinguser.profilePicture,
+          name: existinguser.fname + " " + existinguser.lname,
+          email: existinguser.email,
+          balance: existinguser.balance,
+        },
+        message: "Fetched",
+      });
+    } else {
+      let e = new Error("User is Blocked from using TourBook Services");
+      e.statusCode = 400;
+      return res
+        .status(e?.statusCode || 400)
+        .send({ data: null, message: e.message });
+    }
   } else {
     return res.status(400).send({
       data: null,
