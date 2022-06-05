30088e5b041a99ae6560be8f9def9d98059cad1c
Zain Haroon <zainharoon890@gmail.com>
2022-06-05 10:37:40 +0500

    Update OAuth2.js

--- routes/OAuth2.js
+++ routes/OAuth2.js
@@ -9,6 +9,7 @@ router.post("/google/createuser", async (req, res) => {
     isDeleted: false,
   });
   if (!existinguser) {
+    console.log("INSIDE CREATE");
     let newUser = await UserModel({
       email: email,
       profilePicture: profilePicture,
@@ -33,6 +34,7 @@ router.post("/google/createuser", async (req, res) => {
       message: "Fetched",
     });
   } else if (existinguser?.source == "google") {
+    if(existinguser.isActive){
     let user = {
       id: existinguser._id,
       fname: existinguser.fname,
@@ -51,6 +53,12 @@ router.post("/google/createuser", async (req, res) => {
       },
       message: "Fetched",
     });
+  }
+  else{
+    let e= new Error("User is Blocked from using TourBook Services");
+    e.statusCode=400;
+    return res.status(e?statusCode||400).send({data:null,message:e.message});
+  }
   } else {
     return res.status(400).send({
       data: null,
